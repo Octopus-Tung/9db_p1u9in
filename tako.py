88ec0e2ac27c89d6e24887a3a5bd325f83edb317
Octopus-Tung <asd86520k@gmail.com>
2019-08-06 20:28:56 +0800

    Add cmd : list all link_map addr & show link_map content.

--- tako.py
+++ tako.py
@@ -35,15 +35,24 @@ class Tako(gdb.Command):
 	def __init__(self):
 		super(Tako, self).__init__("Tako", gdb.COMMAND_USER)
 
-	def link_map(self):
+	def link_map(self, arg):
 		main_ELF = re.search("exe.*", gdb.execute("info proc exe", to_string = True)).group().split("'")[1]
 		#DT_DEBUG
 		r_debug = Dyn_entry(main_ELF, 0x15, 1)
-		return gdb.Value(r_debug).cast(gdb.lookup_type("struct link_map").pointer()).dereference()
+		link_map = gdb.Value(r_debug).cast(gdb.lookup_type("struct r_debug").pointer()).dereference()["r_map"]
+		link_map_list = []
+		while link_map != 0:
+			link_map_list.append(link_map)
+			link_map = gdb.Value(link_map).cast(gdb.lookup_type("struct link_map").pointer()).dereference()["l_next"]
+		if arg == "list":
+			for i in link_map_list:
+				print(str(i).split()[0] + ":" + str(gdb.Value(i).cast(gdb.lookup_type("struct link_map").pointer()).dereference()["l_name"]).split()[1])
+		elif int(arg) <= len(link_map_list) and int(arg) > 0:
+			print(str(gdb.Value(link_map_list[int(arg) - 1]).cast(gdb.lookup_type("struct link_map").pointer()).dereference()))
 
 	def invoke(self, args, from_tty):
 		arg = args.split()
-		if arg[0] == "link_map":
-			print(self.link_map())
+		if arg[0] == "link_map" and len(arg) == 2:
+			self.link_map(arg[1])
 
 Tako()
